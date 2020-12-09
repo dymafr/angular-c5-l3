b350a99e14a3b7b62aaa310d8eba61dfff5c961c
dymafr <erwan1309@hotmail.com>
2020-12-09 17:15:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-c5-l3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-c5-l3)
---
+++ src/app/cocktail-container/cocktail-list/cocktail-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Cocktail } from "../../interfaces/cocktail.interface";
+
+@Component({
+  selector: "app-cocktail-list",
+  templateUrl: "./cocktail-list.component.html",
+  styleUrls: ["./cocktail-list.component.scss"]
+})
+export class CocktailListComponent implements OnInit {
+  @Input() public cocktails: Cocktail[];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
